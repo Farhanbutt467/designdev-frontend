2215c5bd755d3a89b659ffa5ef415950f38c048c
Mateen <mateenafzal143@gmail.com>
2026-04-18 15:07:41 +0500

    contact page dynamic

--- src/app/(inner-style-1)/contact/page.tsx
+++ src/app/(inner-style-1)/contact/page.tsx
@@ -3,11 +3,40 @@ import ContactHero from "@/components/contact/ContactHero";
 import Image from "next/image";
 import ContactArea from "@/components/contact/ContactArea";
 import SeoData from "@/components/tools/SeoData";
+import { getImageUrl, getpageData } from "@/lib/helper/api";
 
-const Contact = () => {
+const Contact = async () => {
   const { data: contactData } = getMainPage("/contact/_index.mdx");
 
   const { meta, hero, image, contact: mainContact } = contactData || {};
+  const contactpageData = await getpageData("contact");
+  const contactContent = contactpageData?.content || {};
+
+  // ContactHero Component Data Update from API
+  if (contactContent.hero) {
+    hero.title = contactContent.hero.title || hero.title;
+    hero.direct_contact.title = contactContent.hero.direct_contact.title || hero.direct_contact.title;
+    hero.direct_contact.link = contactContent.hero.direct_contact.link || hero.direct_contact.link;
+  }
+  if (contactContent.hero?.images && contactContent.hero.images.length > 0) {
+    hero.images = contactContent.hero.images.map((img: any, index: number) => getImageUrl(img) || hero.images[index]);
+  }
+
+ const updatedImage = contactContent.image ? getImageUrl(contactContent.image) || image : image;
+
+  // ContactArea Component Data Update from API
+  if (contactContent.header) {
+    mainContact.header.title = contactContent.header.title || mainContact.header.title;
+    mainContact.header.description = contactContent.header.description || mainContact.header.description;
+    mainContact.info.title = contactContent.info.title || mainContact.info.title;
+  }
+
+  if (contactContent.info?.contact_list) {
+  mainContact.info.contact_list.phone = contactContent.info.contact_list.phone || mainContact.info.contact_list.phone;
+  mainContact.info.contact_list.email = contactContent.info.contact_list.email || mainContact.info.contact_list.email;
+  mainContact.info.contact_list.address = contactContent.info.contact_list.address || mainContact.info.contact_list.address;
+  }
+  
   return (
     <main>
       <SeoData
@@ -17,7 +46,7 @@ const Contact = () => {
       <ContactHero {...hero} />
       <div className="overflow-hidden">
         <Image
-          src={image}
+          src={updatedImage}
           alt="image"
           width={1920}
           height={850}
